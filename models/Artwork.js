58a75539742eb7958ca6b46ece10fedf01da8517
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-03-10 18:34:30 +1000

    Fixing Artwork model

--- models/Artwork.js
+++ models/Artwork.js
@@ -3,21 +3,30 @@ const sequelize = require("../config/connection");
 
 class Artwork extends Model {}
 
-Artwork.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+Artwork.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.BLOB("long"),
+      allowNull: false,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  image: {
-    type: DataTypes.BLOB("long"),
-    allowNull: false,
-  },
-});
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "artwork",
+  }
+);
 
 module.exports = Artwork;
