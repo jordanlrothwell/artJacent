7c986eeb0b9f846ea229ef475a44724966e039f9
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-03-14 21:41:42 +1100

    added a helper function to handle the mergeSort algorithm for sorting our posts by relative distance to each user

---
+++ utils/mergeSort.js
@@ -0,0 +1,33 @@
+const merge = (left, right) => {
+    let sortedArr = []; // the sorted elements will go here
+
+  while (left.length && right.length) {
+    // insert the smallest element to the sortedArr
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift());
+    } else {
+      sortedArr.push(right.shift());
+    }
+  }
+
+  // use spread operator and create a new array, combining the three arrays
+  return [...sortedArr, ...left, ...right];
+}
+
+const mergeSort = (arr) => {
+    const half = arr.length / 2;
+
+  // the base case is array length <=1
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const left = arr.splice(0, half); // the first half of the array
+  const right = arr;
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+
+// Set up a test case for the algorithm
+const test = [1,9,7,3,2,5,8,11,16,4];
+console.log(mergeSort(test));
