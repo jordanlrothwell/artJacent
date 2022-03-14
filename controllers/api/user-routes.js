f1efc7adf871e6340a30e8edd91aa58556665a84
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-03-14 19:39:35 +1100

    added logic to handle the profile page when a user logs in

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -46,9 +46,7 @@ router.post("/login", async (req, res) => {
 
     req.session.loggedIn = true;
 
-    res
-      .status(200)
-      .json({ user: dbUserData, message: "You are now logged in!" });
+    res.status(200).json({ user: dbUserData, message: "You are now logged in!" });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -19,11 +19,35 @@ router.get("/", async (req, res) => {
       artwork.get({plain: true})
     );
 
-    res.render("homepage", { art, loggedIn: false });
+    res.render("feed", { art, loggedIn: false });
   } catch (err) {
     res.status(400).json(err);
   }
 });
+
+
+// Get the profile of the user after login
+router.get("/profile", async (req, res) => {
+  try {
+    const dbArtData = await Artwork.findAll({
+      include: [
+        { 
+          model: User,
+          attributes: ["name"],
+        }
+      ]
+    });
+
+    const art = dbArtData.map((artwork) => 
+      artwork.get({plain: true})
+    );
+
+    res.render("profile", {art, loggedIn: false});
+    
+  } catch (err) {
+    res.status(400).json(err)
+  }
+})
 
 // Send the user to the hame page when logged in
 router.get("/login", (req, res) => {
