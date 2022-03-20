3cf4a890770267c3ef904422176b1aef6f38dbfe
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-03-20 20:49:09 +1100

    added logic to loop through every post and create a new object for each of them with the distance between the user and the post - will add ordering functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const { Artwork, User } = require("../models");
-const { withAuth } = require("../utils");
+const { withAuth, distance  } = require("../utils");
 const { findByPk } = require("../models/User");
 
 // Send all items in the artwork table to /
 router.get("/", async (req, res) => {
+  const postsArray = [];
+
   try {
     const dbArtData = await Artwork.findAll({
       include: [
@@ -14,10 +16,39 @@ router.get("/", async (req, res) => {
       ],
     });
 
+
+    const dbUserData = await User.findOne({
+      where: {
+        id: req.session.user_id
+      }
+    })
+
+    // Get the current user's location
+    const user = dbUserData.get({plain: true});
+    const userLatitude = user.latitude;
+    const userLongitude = user.longitude;
+    
+    // Get all artworks
     const art = dbArtData.map((artwork) => artwork.get({ plain: true }));
     console.log(art);
 
-    res.render("feed", { art, logged_in: req.session.logged_in });
+    // For ecery artwork, find the distance of the post to the user
+    for (let i=0; i<art.length; i++) {
+      const postLatitude = art[i].user.latitude;
+      const postLongitude = art[i].user.longitude;
+
+      const obj = {
+        name: art[i].name,
+        image: art[i].image,
+        user: art[i].user.name,
+        distance: distance(userLatitude, userLongitude, postLatitude, postLongitude)
+      }
+      postsArray.push(obj);
+    }
+
+    console.log(postsArray);
+
+    res.render("feed", { postsArray, logged_in: req.session.logged_in });
   } catch (err) {
     res.status(500).json(err);
   }
