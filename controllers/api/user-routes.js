a9477c74c35d2fe85abe384c36547c6a94f25596
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-03-21 15:36:05 +1000

    Added seed routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -21,6 +21,18 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.post("/seed", async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 // login - checks for an existing user and logs in
 router.post("/login", async (req, res) => {
   try {
