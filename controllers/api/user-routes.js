ef9ac776a9ceb5e4d58527d25b2e1e606d1cbbc2
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-03-21 15:36:46 +1000

    Updated seed route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -23,11 +23,11 @@ router.post("/", async (req, res) => {
 
 router.post("/seed", async (req, res) => {
   try {
-    const newPost = await Post.create({
+    const newArtwork = await Artwork.create({
       ...req.body,
     });
 
-    res.status(200).json(newPost);
+    res.status(200).json(newArtwork);
   } catch (err) {
     res.status(400).json(err);
   }
